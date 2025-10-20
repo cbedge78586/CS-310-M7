39f101a7cfe63b076645d9f9f8f60dcc7e55cac9
Charles Edge <cbedge@my365.bellevue.edu>
2025-10-20 06:11:35 -0500

    Add files via upload

---
+++ Edge - M7 Discussion.cpp
@@ -0,0 +1,25 @@
+// Charles Edge
+// CS310-T301
+// M7: Discussion
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// 4x4 table
+    int table[4][4]; 
+
+// should fill table with values listed below
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            table[i][j] == i + p; 
+        }
+    }
+// Declare integer
+    int z = 2;
+
+// Prints output to screen
+    cout << "Value at (2,2): " << table[k][2] << endl;
+
+    return 0;
+}
